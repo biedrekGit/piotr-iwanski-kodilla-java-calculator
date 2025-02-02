e60559e47b91a5ef878e8e583d9ada9062fea70f
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-02-02 13:09:38 +0100

    Third commit

--- src/main/java/com/kodilla/kodillacalculator/Calculator.java
+++ src/main/java/com/kodilla/kodillacalculator/Calculator.java
@@ -2,22 +2,34 @@ package com.kodilla.kodillacalculator;
 
 public class Calculator {
 
-    public int addition(int a, int b) {
+    public double addition(double a, double b) {
         return a + b;
     }
 
-    public int subtraction(int a, int b) {
+    public double subtraction(double a, double b) {
         return a -b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
     }
 
     public static void main(String[] args)  {
         Calculator calculator = new Calculator();
-        int a = 67;
-        int b = 23;
-        int addResult = calculator.addition(a, b);
-        int subResult = calculator.subtraction(a, b);
+        double a = 67;
+        double b = 23;
+        double addResult = calculator.addition(a, b);
+        double subResult = calculator.subtraction(a, b);
+        double mulResult = calculator.multiplication(a, b);
+        double divResult = calculator.division(a, b);
 
         System.out.println(a + " plus " + b + " equals: " + addResult);
         System.out.println(a + " minus " + b + " equals: " + subResult);
+        System.out.println(a + " times " + b + " equals: " + mulResult);
+        System.out.println(a + " divided by " + b + " equals: " + divResult);
     }
 }
